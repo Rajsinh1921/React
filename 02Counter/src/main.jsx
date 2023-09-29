ce6f93df024561d845489d31d2058078162fc2eb
Rajsinh1921 <143887210+Rajsinh1921@users.noreply.github.com>
2023-09-29 16:25:37 +0530

    second commit

--- 02Counter/src/main.jsx
+++ 02Counter/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MSM from './Try'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MSM/> x
+    <App />
   </React.StrictMode>,
 )
