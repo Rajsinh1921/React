33f72e68b4483fcfd5341df33e47030b00d52739
Rajsinh1921 <143887210+Rajsinh1921@users.noreply.github.com>
2023-10-01 12:57:33 +0530

    V.02

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -7,8 +7,57 @@ function App() {
 
   return (
     <>
-      <div 
-      ></div>
+      <div className='w-full h-screen'
+      style = {{backgroundColor : color}}
+      >
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+            <button 
+            onClick={() => setcolor('red')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : "red" }} 
+            >Red</button>
+            <button 
+            onClick={() => setcolor('blue')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : "blue" }} 
+            >Blue</button>
+            <button 
+            onClick={() => setcolor('black')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : "black" }} 
+            >Black</button>
+            <button 
+            onClick={() => setcolor('white')}
+            className='outline-none px-4 py-1 rounded-full text-black shadow-lg'
+            style = {{backgroundColor : "white" }} 
+            >White</button>
+            <button 
+            onClick={() => setcolor('purple')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : "purple" }} 
+            >purple</button>
+            <button 
+            onClick={() => setcolor('Pink')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : "Pink" }} 
+            >Pink</button>
+            <button 
+            onClick={() => setcolor('gray')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : "gray" }} 
+            >Gray</button>
+            <button 
+            onClick={() => setcolor('indigo')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : "indigo" }} 
+            >Indigo</button>
+          </div>
+        </div>
+
+
+
+      </div>
     </>
   )
 }
