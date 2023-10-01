70181e3086f436c8a4d5f644a2c4669f430c7c16
Rajsinh1921 <143887210+Rajsinh1921@users.noreply.github.com>
2023-10-01 09:56:10 +0530

    bgcolor

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+    const [color, setcolor] = useState("orange")
+
+  return (
+    <>
+      <div 
+      ></div>
+    </>
+  )
+}
+
+export default App
