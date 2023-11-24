63d041fe3078ce232b8115350e6e701cb7ff7133
Rajsinh Chauhan <143887210+Rajsinh1921@users.noreply.github.com>
2023-11-24 23:39:55 +0530

    UI completed

---
+++ 09Themetoggler/src/App.jsx
@@ -0,0 +1,25 @@
+import Card from "./Components/Card";
+import ThemeBtn from "./Components/ThemeBtn";
+import { ThemeProvider } from "./Context/Themecontex";
+
+function App() {
+  return (
+    <>
+      <ThemeProvider value={{ themeMode, setThemeMode }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn />
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
