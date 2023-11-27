e46c0d42c8397deb909ed42ccb294f7db4f411fc
Rajsinh Chauhan <143887210+Rajsinh1921@users.noreply.github.com>
2023-11-27 16:41:31 +0530

    env Setup completed

---
+++ 12MegaProjec/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Jay swaminarayan</h1>
+      {console.log(import.meta.env.VITE_APPWRITE_URL)}
+    </>
+  );
+}
+
+export default App;
