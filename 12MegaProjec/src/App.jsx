198993372cbe06a303aea297413f4aa7273e79a3
Rajsinh Chauhan <143887210+Rajsinh1921@users.noreply.github.com>
2023-11-28 23:08:20 +0530

    Components Added

--- 12MegaProjec/src/App.jsx
+++ 12MegaProjec/src/App.jsx
@@ -1,11 +1,36 @@
-import "./App.css";
-
+import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import authService from "./appwrite/auth";
+import { login } from "./Store/authSlice";
+import LoadingSpinner from "./Components/LoadingSpinner/LoadingSpinner";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch;
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <>
-      <h1>Jay swaminarayan</h1>
-      {console.log(import.meta.env.VITE_APPWRITE_URL)}
-    </>
+    <center className="">
+      {loading && <LoadingSpinner />}
+
+      <div className="min-h-screen flex flex-wrap contents-between bg-gray-500">
+        <div className="w-full block">
+          <Header />
+          <Footer />
+        </div>
+      </div>
+    </center>
   );
 }
 
---
+++ 12MegaProjec/src/Components/index.js
@@ -0,0 +1,23 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./Container/Container";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Button from "./Button.jsx";
+import { Input } from "./Input.jsx";
+import Login from "./Login.jsx";
+import PostCard from "./PostCard.jsx";
+import Select from "./Select.jsx";
+
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  Button,
+  Input,
+  Login,
+  PostCard,
+  Select,
+};
